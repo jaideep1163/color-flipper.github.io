55affb05a17e6f42b9afbc06523125f8fac9405e
jaideep1163 <66325920+jaideep1163@users.noreply.github.com>
2021-03-21 10:09:04 +0530

    Hex Completed

---
+++ hex.js
@@ -0,0 +1,21 @@
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+// Hexadecimal
+// #f15025
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", () => {
+    let hexColor = "#";
+    for (let i = 0; i < 6; i++) {
+        hexColor += hex[getRandomNumbers()];
+    }
+    
+    color.textContent = hexColor;
+    document.body.style.backgroundColor = hexColor;
+});
+
+function getRandomNumbers() {
+  return Math.floor(Math.random() * hex.length);
+}
